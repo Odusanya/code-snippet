e4e81292ebe2780538443cdebe69ced041aa1e09
Odusanya 'Tomi <odusanya.tomi@gmail.com>
2020-10-06 03:22:41 +0100

    chore: demo without persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import recoilPersist from 'recoil-persist';
 
 import './index.css';
 import App from './App';
-const { RecoilPersist, updateState } = recoilPersist();
-
-
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<RecoilRoot initializeState={updateState}>
-				<RecoilPersist />
+			<RecoilRoot>
 				<App />
 			</RecoilRoot>
 		</BrowserRouter>
