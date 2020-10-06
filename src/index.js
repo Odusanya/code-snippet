7271eeab28615ad156057848350686c5f16d8552
Odusanya 'Tomi <odusanya.tomi@gmail.com>
2020-10-06 03:01:29 +0100

    chore: remove unused code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import recoilPersist from 'recoil-persist';
 
 import './index.css';
 import App from './App';
-const { RecoilPersist, updateState } = recoilPersist();
-
-
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<RecoilRoot initializeState={updateState}>
-				<RecoilPersist />
-				<App />
-			</RecoilRoot>
+			<App />
 		</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
